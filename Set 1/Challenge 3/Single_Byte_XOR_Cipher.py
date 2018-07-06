4bd0d80f9d82fcf5d0931f3c55aa3fcd1d999c55
Connor Leavesley <35899720+clev98@users.noreply.github.com>
2018-07-06 16:43:24 -0400

    Update Single_Byte_XOR_Cipher.py

--- Set 1/Challenge 3/Single_Byte_XOR_Cipher.py
+++ Set 1/Challenge 3/Single_Byte_XOR_Cipher.py
@@ -16,5 +16,5 @@ def breakSingleByteXOR(binary):
 
 if __name__ == "__main__":
     hexStr = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-    analysis = bruteForce(unhexlify(hexStr))
+    analysis = breakSingleByteXOR(unhexlify(hexStr))
     print(max(analysis, key=lambda s: s.count(' ')))
