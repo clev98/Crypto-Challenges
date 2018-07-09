e6d5a18b309170df0cb9fd44a86dc6ce2b008fb8
Connor Leavesley <35899720+clev98@users.noreply.github.com>
2018-07-09 12:20:03 -0400

    Update Repeating_Key_XOR.py

--- Set 1/Repeating_Key_XOR.py
+++ Set 1/Repeating_Key_XOR.py
@@ -6,11 +6,11 @@ def repeatingKeyXOR(text, key):
     for n in range(len(text)):
         xorString += chr(text[n]^key[n])
 
-    return xorString.encode('utf-8').hex()
+    return xorString.encode('utf-8')
 
 if __name__ == "__main__":
     text = """Burning 'em, if you ain't quick and nimble
 I go crazy when I hear a cymbal""".encode('utf-8')
     key = "ICE".encode('utf-8')
 
-    print(repeatingKeyXOR(text, key))
+    print(repeatingKeyXOR(text, key).hex())
