bdc037caefa0b5ad1ff6875e266a613b75eddb44
Connor Leavesley <35899720+clev98@users.noreply.github.com>
2018-07-13 14:27:13 -0400

    Add files via upload

---
+++ Set 2/ECB_Cut_And_Paste.py
@@ -0,0 +1,80 @@
+#ECB cut and paste
+from os import urandom
+from Crypto.Cipher import AES
+
+AESKey = urandom(AES.block_size)
+
+def kvparsing(kv):
+    kvdict = {}
+    kv = kv.split("&".encode('utf-8'))
+
+    for pair in kv:
+        pair = pair.split("=".encode('utf-8'))
+        kvdict[pair[0]] = pair[1]
+
+    return kvdict
+
+def profileFor(email): #oracle
+    assert not ("&".encode('utf-8') in email) and not ("=".encode('utf-8') in email)
+    encoded = ("email=".encode('utf-8'))+email+("&uid=10&role=user".encode('utf-8'))
+    return encryptAES_ECB(encoded, AESKey)
+
+def encryptAES_ECB(plaintext, key):
+    return bytearray(AES.new(key, AES.MODE_ECB).encrypt(addPKCS7Padding(plaintext, AES.block_size, "\x04".encode('utf-8'))))
+
+def decryptAES_ECB(ciphertext, key):
+    return AES.new(key, AES.MODE_ECB).decrypt(ciphertext)
+
+def addPKCS7Padding(text, blockSize, paddingChar):
+    if len(text) % blockSize:
+        neededPadding = blockSize - (len(text) % blockSize)
+        
+        for n in range(neededPadding):
+            text += paddingChar
+            
+    return text
+
+def removePKCS7Padding(text, paddingChar):
+    for n in range(len(text)-1, -1, -1):
+        if text[n] == ord(paddingChar):
+            text.remove(ord(paddingChar))
+            
+    return text
+
+def findBlocksize(oracle):
+    unknownTextLength = len(oracle(bytearray()))
+    i = 1
+
+    while True:
+        newTextLength = len(oracle(bytearray("A" * i, 'utf-8')))
+        
+        if newTextLength - unknownTextLength:
+            return newTextLength - unknownTextLength
+        i += 1
+
+if __name__ == "__main__":
+    blockSize = findBlocksize(profileFor)
+    currentBytes = len("email=&uid=10&role=")
+
+    if currentBytes % blockSize != 0:
+        neededBytes = blockSize - currentBytes % blockSize
+    else:
+        neededBytes = 0
+
+    email = bytearray("A"*neededBytes, 'utf-8')
+    prefix = profileFor(email)[:neededBytes + currentBytes]
+    currentBytes = len("email=")
+
+    if currentBytes % blockSize != 0:
+        neededBytes = blockSize - currentBytes % blockSize
+    else:
+        neededBytes = 0
+
+    email = bytearray("A"*neededBytes, 'utf-8') + addPKCS7Padding("admin".encode('utf-8'), blockSize, "\x04".encode('utf-8'))
+    postfix = profileFor(email)[currentBytes + neededBytes:2*blockSize]
+    adminUser = prefix + postfix
+
+    kvpairs = kvparsing(decryptAES_ECB(adminUser, AESKey))
+
+    for item in kvpairs.keys():
+        print(item.decode('utf-8')+": "+kvpairs[item].decode('utf-8'))
