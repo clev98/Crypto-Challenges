80d6d15aba367604d80834d23d5358b6e433be76
Connor Leavesley <35899720+clev98@users.noreply.github.com>
2018-08-01 17:47:35 -0400

    Add files via upload

---
+++ Set 2/PKCS7_Padding_Validation.py
@@ -0,0 +1,15 @@
+#Validate PKCS7 Padding
+from Cryptodome.Cipher import AES
+
+def testPadding(text, blocksize):
+    if len(text) % blocksize:
+        raise Exception("Invalid PKCS7 Padding")
+    
+    for n in range(len(text) - 1, len(text) - text[-1], -1):
+        if text[n] != text[-1]:
+            raise Exception("Invalid PKCS7 Padding")
+
+    return text
+
+if __name__ == "__main__":
+    print(testPadding("ICE ICE BABY\x04\x04\x04\x04".encode('utf-8'), AES.block_size))
