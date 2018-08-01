818ae346afeb2332515e2de2ca3b659ff419e9ce
Connor Leavesley <35899720+clev98@users.noreply.github.com>
2018-08-01 16:04:37 -0400

    Whoops

--- Set 2/Implement_PKCS#7_Padding.py
+++ Set 2/Implement_PKCS#7_Padding.py
@@ -1,25 +1,22 @@
 #Implement PKCS#7 Padding
-def addPKCS7Padding(text, blockSize, paddingChar):
+def addPKCS7Padding(text, blockSize):
     if len(text) % blockSize:
-        neededPadding = blockSize - (len(text) % blockSize)
-        
-        for n in range(neededPadding):
-            text += paddingChar
+        neededPadding = blockSize - len(text) % blockSize
+        text += bytearray((chr(neededPadding)*neededPadding), 'utf-8')
             
     return text
 
-def removePKCS7Padding(text, paddingChar):
-    for n in range(len(text)-1, -1, -1):
-        if text[n] == ord(paddingChar):
-            text.remove(ord(paddingChar))
-            
-    return text
+def removePKCS7Padding(text, blockSize):
+    for n in range(len(text) - 1, len(text) - text[-1] - 1, -1):
+        if text[n] != text[-1]:
+            return text
+
+    return text[:-text[-1]]
 
 if __name__ == "__main__":
     text = bytearray("YELLOW SUBMARINE", 'utf-8')
-    paddingChar = bytearray("\x04", 'utf-8')
     
-    addedPKCS = addPKCS7Padding(text, 20, paddingChar)
+    addedPKCS = addPKCS7Padding(text, 20)
     print(addedPKCS)
-    removedPKCS = removePKCS7Padding(addedPKCS, paddingChar)
+    removedPKCS = removePKCS7Padding(addedPKCS, 20)
     print(removedPKCS)
