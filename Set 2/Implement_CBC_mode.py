6fc78a42303ce47d2c3ddac3c27994df9c181541
Connor Leavesley <35899720+clev98@users.noreply.github.com>
2018-08-01 16:13:54 -0400

    Update Implement_CBC_mode.py

--- Set 2/Implement_CBC_mode.py
+++ Set 2/Implement_CBC_mode.py
@@ -1,5 +1,5 @@
-#Implement AES_ECB_CBC mode
-from Crypto.Cipher import AES
+#Implement CBC mode
+from Cryptodome.Cipher import AES
 from binascii import a2b_base64
 
 def readFile(path):
@@ -8,21 +8,19 @@ def readFile(path):
 
     return ciphertext
 
-def addPKCS7Padding(text, blockSize, paddingChar):
+def addPKCS7Padding(text, blockSize):
     if len(text) % blockSize:
-        neededPadding = blockSize - (len(text) % blockSize)
-        
-        for n in range(neededPadding):
-            text += paddingChar
+        neededPadding = blockSize - len(text) % blockSize
+        text += bytearray((chr(neededPadding)*neededPadding), 'utf-8')
             
     return text
 
-def removePKCS7Padding(text, paddingChar):
-    for n in range(len(text)-1, -1, -1):
-        if text[n] == ord(paddingChar):
-            text.remove(ord(paddingChar))
-            
-    return text
+def removePKCS7Padding(text, blockSize):
+    for n in range(len(text) - 1, len(text) - text[-1] - 1, -1):
+        if text[n] != text[-1]:
+            return text
+
+    return text[:-text[-1]]
 
 def decryptAES_ECB(ciphertext, key):
     return AES.new(key, AES.MODE_ECB).decrypt(ciphertext)
@@ -34,13 +32,13 @@ def decryptAES_ECB_CBC(ciphertext, key, iv):
         plaintext[n: n+AES.block_size] = xor(decryptAES_ECB(ciphertext[n: n+AES.block_size], key), iv)
         iv = ciphertext[n: n+AES.block_size]
 
-    return removePKCS7Padding(plaintext, "\x04".encode('utf-8'))
+    return removePKCS7Padding(plaintext, AES.block_size)
 
 def encryptAES_ECB(plaintext, key):
     return AES.new(key, AES.MODE_ECB).encrypt(plaintext)
 
 def encryptAES_ECB_CBC(plaintext, key, iv):
-    plaintext = addPKCS7Padding(plaintext, AES.block_size, "\x04".encode('utf-8'))
+    plaintext = addPKCS7Padding(plaintext, AES.block_size)
     ciphertext = bytearray(len(plaintext))
 
     for n in range(0, len(text), AES.block_size):
