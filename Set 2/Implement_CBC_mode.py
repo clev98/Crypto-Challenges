d13b5609070e118bfb2759a75f9ebb7abbe1ff1c
Connor Leavesley <35899720+clev98@users.noreply.github.com>
2018-08-03 13:04:51 -0400

    Update Implement_CBC_mode.py

--- Set 2/Implement_CBC_mode.py
+++ Set 2/Implement_CBC_mode.py
@@ -1,6 +1,10 @@
-#Implement CBC mode
+# Implement CBC mode
 from Cryptodome.Cipher import AES
 from binascii import a2b_base64
+from Set1.Fixed_XOR import xor
+from Set1.AES_in_ECB_mode import decryptAES_ECB, encryptAES_ECB
+from Implement_PKCS7_Padding import addPKCS7Padding, removePKCS7Padding
+
 
 def readFile(path):
     with open(path) as file:
@@ -8,22 +12,6 @@ def readFile(path):
 
     return ciphertext
 
-def addPKCS7Padding(text, blockSize):
-    if len(text) % blockSize:
-        neededPadding = blockSize - len(text) % blockSize
-        text += bytearray((chr(neededPadding)*neededPadding), 'utf-8')
-            
-    return text
-
-def removePKCS7Padding(text, blockSize):
-    for n in range(len(text) - 1, len(text) - text[-1] - 1, -1):
-        if text[n] != text[-1]:
-            return text
-
-    return text[:-text[-1]]
-
-def decryptAES_ECB(ciphertext, key):
-    return AES.new(key, AES.MODE_ECB).decrypt(ciphertext)
 
 def decryptAES_ECB_CBC(ciphertext, key, iv):
     plaintext = bytearray(len(ciphertext))
@@ -34,8 +22,6 @@ def decryptAES_ECB_CBC(ciphertext, key, iv):
 
     return removePKCS7Padding(plaintext, AES.block_size)
 
-def encryptAES_ECB(plaintext, key):
-    return AES.new(key, AES.MODE_ECB).encrypt(plaintext)
 
 def encryptAES_ECB_CBC(plaintext, key, iv):
     plaintext = addPKCS7Padding(plaintext, AES.block_size)
@@ -47,21 +33,14 @@ def encryptAES_ECB_CBC(plaintext, key, iv):
 
     return ciphertext
 
-def xor(string1, string2):
-    text = bytearray(len(string1))
-
-    for i in range(len(string1)):
-        text[i] = string1[i]^string2[i]
-
-    return text
 
 if __name__ == "__main__":
-    text = readFile("10.txt")
+    text = readFile(r"C:\Users\Connor\Desktop\Code\Python3\Crypto\Set 2\10.txt")
     key = "YELLOW SUBMARINE".encode('utf-8')
-    #iv being the initialization vector 
+    # iv being the initialization vector
     iv = ("\x00"*AES.block_size).encode('utf-8')
     print(decryptAES_ECB_CBC(text, key, iv))
 
-    #Test
+    # Test
     #plaintext = bytearray("Please work code", 'utf-8')
     #print(decryptAES_ECB_CBC(encryptAES_ECB_CBC(plaintext, key, iv), key, iv))
