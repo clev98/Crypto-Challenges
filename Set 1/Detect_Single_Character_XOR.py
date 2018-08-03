c68cdc8f8047bb863774195b417ecd3f6d1e5c9e
Connor Leavesley <35899720+clev98@users.noreply.github.com>
2018-08-03 12:46:10 -0400

    Update Detect_Single_Character_XOR.py

--- Set 1/Detect_Single_Character_XOR.py
+++ Set 1/Detect_Single_Character_XOR.py
@@ -1,35 +1,31 @@
-#Detect a string from a list of sixty that has been encrypted by a
-#single character XOR.
+# Detect a string from a list of sixty that has been encrypted by a
+# single character XOR.
 from binascii import unhexlify
+from Break_Single_Byte_XOR import breakSingleByteXOR
+
 
 def readFile(file):
     strList = []
-    
+
     with open(file) as data:
         for line in data:
             strList.append(line.strip())
 
     return strList
 
-def breakSingleByteXOR(byteStr):
-    strings = []
-    
-    for key in range(256):
-        string = ""
-        
-        for num in byteStr:
-            string += chr(num^key)
-
-        strings.append(string)
-
-    return max(strings, key=lambda s: s.count(' ')).strip('\n')
 
 if __name__ == "__main__":
-    strList = readFile("4.txt")
+    strList = readFile(r"C:\Users\Connor\Desktop\Code\Python3\Crypto\Set 1\4.txt")
 
     for string in strList:
         possibleSoln = breakSingleByteXOR(unhexlify(string))
 
-        if possibleSoln.isprintable():
-            print(possibleSoln)
-            break
+        try:
+            possibleSoln = possibleSoln.decode('utf-8').strip("\n")
+
+            if possibleSoln.isprintable():
+                print(possibleSoln)
+                break
+
+        except UnicodeDecodeError:
+            continue
