26383617efdd145aa96f985b440c8881f4e62d8d
Connor Leavesley <35899720+clev98@users.noreply.github.com>
2018-08-03 12:30:13 -0400

    Update Break_Single_Byte_XOR.py

--- Set 1/Break_Single_Byte_XOR.py
+++ Set 1/Break_Single_Byte_XOR.py
@@ -1,19 +1,17 @@
-#Break single byte XOR
-#Take all possible solns and take the one with the most spaces (' ')
+# Break single byte XOR
+# Take all possible solns and take the one with the most spaces (' ')
 from binascii import unhexlify
+from Fixed_XOR import xor
+
 
 def breakSingleByteXOR(byteStr):
     strings = []
-    
-    for key in range(256):
-        string = bytearray(len(byteStr))
-        
-        for num in range(len(byteStr)):
-            string[num] = byteStr[num]^key
 
-        strings.append(string)
+    for key in range(256):
+        strings.append(xor(byteStr, bytearray((chr(key)*len(byteStr)), 'utf-8')))
 
     return max(strings, key=lambda s: s.count(' '.encode('utf-8')))
+
 
 if __name__ == "__main__":
     hexStr = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
