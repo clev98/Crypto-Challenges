d8567d0f77c6b86233bf6e80efb3af1ef289e7be
Connor Leavesley <35899720+clev98@users.noreply.github.com>
2018-08-07 13:14:45 -0400

    Update Implement_PKCS7_Padding.py

--- Set 2/Implement_PKCS7_Padding.py
+++ Set 2/Implement_PKCS7_Padding.py
@@ -1,10 +1,13 @@
-#Implement PKCS#7 Padding
+# Implement PKCS#7 Padding
 def addPKCS7Padding(text, blockSize):
     if len(text) % blockSize:
         neededPadding = blockSize - len(text) % blockSize
         text += bytearray((chr(neededPadding)*neededPadding), 'utf-8')
-            
+    else:
+        text += bytearray((chr(blockSize)*blockSize), 'utf-8')
+
     return text
+
 
 def removePKCS7Padding(text, blockSize):
     for n in range(len(text) - 1, len(text) - text[-1] - 1, -1):
@@ -13,10 +16,11 @@ def removePKCS7Padding(text, blockSize):
 
     return text[:-text[-1]]
 
+
 if __name__ == "__main__":
     text = bytearray("YELLOW SUBMARINE", 'utf-8')
-    
-    addedPKCS = addPKCS7Padding(text, 20)
+
+    addedPKCS = addPKCS7Padding(text, 16)
     print(addedPKCS)
-    removedPKCS = removePKCS7Padding(addedPKCS, 20)
+    removedPKCS = removePKCS7Padding(addedPKCS, 16)
     print(removedPKCS)
