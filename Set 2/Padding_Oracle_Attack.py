33462797355dbf319c908f816764822f200c418c
Connor Leavesley <35899720+clev98@users.noreply.github.com>
2018-08-11 13:15:31 -0400

    Update Padding_Oracle_Attack.py

--- Set 2/Padding_Oracle_Attack.py
+++ Set 2/Padding_Oracle_Attack.py
@@ -1,8 +1,9 @@
+#Basic demonstration of a Padding Oracle Attack
 from Cryptodome.Cipher import AES
 from base64 import b64decode
 from os import urandom
-from Set1.AES_in_ECB_mode import encryptAES_ECB
-from Set1.Detect_AES_in_ECB_mode import detectECB
+from AES_in_ECB_mode import encryptAES_ECB
+from Detect_AES_in_ECB_mode import detectECB
 from Implement_PKCS7_Padding import addPKCS7Padding, removePKCS7Padding
 
 RandomAESKey = urandom(AES.block_size)
@@ -14,7 +15,6 @@ def oracle(knownText):
 
 
 def byteAtATime():
-    blockSize = findBlocksize()
     roundedStringSize = len(oracle(bytearray()))
 
     unknownString = bytearray()
@@ -30,19 +30,7 @@ def byteAtATime():
                 unknownString += chr(j).encode('utf-8')
                 break
 
-    return removePKCS7Padding(unknownString, blockSize)
-
-
-def findBlocksize():
-    unknownTextLength = len(oracle(bytearray()))
-    i = 1
-
-    while True:
-        newTextLength = len(oracle(bytearray("A" * i, 'utf-8')))
-
-        if newTextLength - unknownTextLength:
-            return newTextLength - unknownTextLength
-        i += 1
+    return removePKCS7Padding(unknownString)
 
 
 if __name__ == "__main__":
