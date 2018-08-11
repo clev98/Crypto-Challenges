134b4e072a4609d694fec10415c5a1bdbee23f50
Connor Leavesley <35899720+clev98@users.noreply.github.com>
2018-08-11 13:02:56 -0400

    Update AES_in_ECB_mode.py

--- Set 1/AES_in_ECB_mode.py
+++ Set 1/AES_in_ECB_mode.py
@@ -1,20 +1,19 @@
-#Decrypts and encrypts AES in ECB mode given a key
-from Crypto.Cipher import AES
+# Decrypts and encrypts AES in ECB mode given a key
+from Cryptodome.Cipher import AES
 from binascii import a2b_base64
 
-def readFile(path):
-    with open(path) as file:
-        ciphertext = a2b_base64(''.join(file.readlines()))
-
-    return ciphertext
 
 def decryptAES_ECB(ciphertext, key):
     return AES.new(key, AES.MODE_ECB).decrypt(ciphertext)
 
+
 def encryptAES_ECB(ciphertext, key):
     return AES.new(key, AES.MODE_ECB).encrypt(ciphertext)
 
+
 if __name__ == "__main__":
-    ciphertext = readFile("7.txt")
+    with open(r"7.txt") as file:
+        ciphertext = a2b_base64(''.join(file.readlines()))
+
     key = "YELLOW SUBMARINE".encode('utf-8')
     print(decryptAES_ECB(ciphertext, key))
